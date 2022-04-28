2abdfce87ec6b271a673b6c2bebc704819fb779b
useragents <103281345+useragents@users.noreply.github.com>
2022-04-28 21:13:32 +0100

    Add files via upload

---
+++ token_checker.js
@@ -0,0 +1,36 @@
+const lineReader = require('line-reader');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fs = require('fs')
+
+console.clear();
+
+function check_token(token) {
+    var req = new XMLHttpRequest();
+    req.open("GET", "https://discord.com/api/v9/users/@me/library", false);
+    req.setRequestHeader("Authorization", token);
+    req.send(null);
+    var status_code = req.status;
+    if (status_code === 401) {
+        console.log("Bad token:", token)
+    }
+    else if (status_code == 200) {
+        console.log("Good token:", token)
+        const content = token + "\n";
+        fs.appendFile('Good.txt', content, err => {
+            if (err) {
+              console.log("[!] Error saving good token to file.")
+              return
+            }
+          })
+    }
+    else if (status_code == 403) {
+        console.log("Locked token:", token)
+    }
+    else {
+        console.log("Unknown error with token:", token)
+    }
+
+}
+lineReader.eachLine('./tokens.txt',(line,last)=>{
+    check_token(line)
+})
